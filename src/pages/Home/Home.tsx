bcebb07218a4984e72dc56575a4345ee6883531a
Ravinder <ravinder.s.negi@indusnet.co.in>
2023-11-24 14:43:08 +0530

    video added

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,12 +4,21 @@ import styles from "./style.module.scss";
 import DetailContainer from "../../components/Home/DetailContainer/DetailContainer";
 import ImageContainer from "../../components/Home/ImageContainer/ImageContainer";
 import AddGroupModal from "../../components/AddGroupModal/AddGroupModal";
+import video from "../../assets/intro.mp4";
 function Home() {
   const [modalStatus, setModalStatus] = useState(false);
   const handleModal = () => {
     setModalStatus(!modalStatus);
   };
-  return (
+  const [flag, setFlag] = useState(true);
+  return flag ? (
+    <div style={{ width: "100%", height: "100vh", background: "#E8E8E8" }}>
+      <video width="100%" height="100%" autoPlay muted>
+        <source src={video} type="video/mp4" width="100%" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  ) : (
     <div className={styles.container}>
       <HomeNav handleModal={handleModal} />
       <div className={styles.body}>
@@ -20,8 +29,7 @@ function Home() {
           <ImageContainer />
         </div>
       </div>
-
-      <AddGroupModal status={modalStatus} handleModal={handleModal} />
+      <AddGroupModal status={modalStatus} handleModal={handleModal} />{" "}
     </div>
   );
 }
