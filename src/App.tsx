aeef067f2f28a8a991d0da261fe223120771ed0d
Ravinder <rvnegi786@gmail.com>
2025-01-24 15:49:23 +0530

    checking

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   const [flag, setFlag] = useState(true);
-  console.log("🚀  flag:", flag)
 
   useEffect(() => {
     setTimeout(() => {
