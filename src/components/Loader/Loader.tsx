f35f890396662129d923447571f45dcb8222f90a
Ravinder <rvnegi786@gmail.com>
2025-02-26 18:33:01 +0530

    new loader added

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -4,16 +4,54 @@ import "./style.css";
 function Loader() {
   return (
     <div className="card">
-      <div className="loader">
-        <p>...loading</p>
-        <div className="words">
-          <span className="word">Groups</span>
-          <span className="word">Expenses</span>
-          <span className="word">Bills</span>
-          <span className="word">Balances</span>
-          <span className="word">calculations</span>
-        </div>
-      </div>
+      <svg className="pl" width="240" height="240" viewBox="0 0 240 240">
+        <circle
+          className="pl__ring pl__ring--a"
+          cx="120"
+          cy="120"
+          r="105"
+          fill="none"
+          stroke="#000"
+          strokeWidth="20"
+          strokeDasharray="0 660"
+          strokeDashoffset="-330"
+          strokeLinecap="round"
+        ></circle>
+        <circle
+          className="pl__ring pl__ring--b"
+          cx="120"
+          cy="120"
+          r="35"
+          fill="none"
+          stroke="#000"
+          strokeWidth="20"
+          strokeDasharray="0 220"
+          strokeDashoffset="-110"
+          strokeLinecap="round"
+        ></circle>
+        <circle
+          className="pl__ring pl__ring--c"
+          cx="85"
+          cy="120"
+          r="70"
+          fill="none"
+          stroke="#000"
+          strokeWidth="20"
+          strokeDasharray="0 440"
+          strokeLinecap="round"
+        ></circle>
+        <circle
+          className="pl__ring pl__ring--d"
+          cx="155"
+          cy="120"
+          r="70"
+          fill="none"
+          stroke="#000"
+          strokeWidth="20"
+          strokeDasharray="0 440"
+          strokeLinecap="round"
+        ></circle>
+      </svg>
     </div>
   );
 }
