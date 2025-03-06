564960539c74f5f63cc182b81442ce4fecf0858a
Ravinder <rvnegi786@gmail.com>
2025-03-06 22:39:19 +0530

    new UI

---
+++ src/components/CustomAccordian/CustomAccordian.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode, useEffect, useState } from "react";
+import { Accordion, AccordionItem as Item } from "@szhsin/react-accordion";
+import styles from "./style.module.css";
+import { ChevronDown } from "lucide-react";
+
+type AccordionItemProps = {
+  header: string;
+  children: ReactNode;
+};
+
+const AccordionItem: React.FC<AccordionItemProps & { defaultOpen?: boolean }> = ({ header, children, defaultOpen, ...rest }) => (
+  <Item
+    {...rest}
+    header={
+      <>
+        {header} <ChevronDown className={styles.chevron} />
+      </>
+    }
+    contentProps={{ className: styles.itemContent }}
+    buttonProps={{
+      className: ({ isEnter }) => `${styles.itemBtn} ${isEnter && styles.itemBtnExpanded}`,
+    }}
+    initialEntered={defaultOpen}
+  >
+    {children}
+  </Item>
+);
+
+const CustomAccordion = (props: AccordionItemProps) => {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsDesktop(window.innerWidth >= 1024);
+    };
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  return (
+    <Accordion transition transitionTimeout={300}>
+      <AccordionItem header={props.header} defaultOpen={isDesktop}>
+        {props.children}
+      </AccordionItem>
+    </Accordion>
+  );
+};
+
+export default CustomAccordion;
