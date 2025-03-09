fd4380368ff4e39d3394ed0c383e3f7454c33f27
Ravinder <rvnegi786@gmail.com>
2025-03-09 09:14:34 +0530

    new

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const port = config.PORT;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:8877", "https://hisabkar.com"],
+  origin: ["http://localhost:8877", "https://hisabkar.com", "https://hisabkar-server.vercel.app"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   optionsSuccessStatus: 200,
   credentials: true, // Enable credentials (cookies, authorization headers) if needed
